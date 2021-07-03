4f825bfe52550cf6b84aecf973bcca2a14c6a201
khalidsy90 <khalidhamedi3@gmail.com>
2021-07-04 00:36:30 +0300

    add updates

--- js/app.js
+++ js/app.js
@@ -1,35 +1,4 @@
-// let inp=document.querySelector('input[name=granulatedsugar]')
-// let lab=document.querySelector('label[for=granulatedsugar]')
-
-// let inp2=document.querySelector('input[name=eggyolks]')
-// let lab2=document.querySelector('label[for=eggyolks]')
-    
-// inp.addEventListener('change',function(event){
-//     let str=lab.textContent
-//     if(this.checked == true)
-//     {
-
-//        console.log(inp.name);
-//        lab.innerHTML=str.strike()
-       
-//     }
-//     else{
-//         lab.innerHTML=str
-//     }
-// })
-
-// inp2.addEventListener('change',function(event){
-//     let str=lab2.textContent
-//     if(this.checked == true)
-//     {
-//        lab2.innerHTML=str.strike()
-//     }
-//     else{
-//         lab2.innerHTML=str
-//     }
-// })
-
-
+// function to do strike for checkboxes
 function doStrik(event){
     let inp=document.querySelector(`#${event.target.id}`)
     let lab=document.querySelector(`label[for=${event.target.id}]`)
